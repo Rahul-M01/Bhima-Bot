586fe75e5b8d0ece9eaee6eabc18ae2d84193567
mahajar2 <rahul.mahajan2@mail.dcu.ie>
2022-04-23 23:31:50 +0100

    Initial commit, with music functionality

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import discord
+from discord.ext import commands
+import requests
+import json
+# from perspective import PerspectiveAPI
+from googleapiclient import discovery
+from discord.ext.commands import has_permissions, MissingPermissions
+import music
+from decouple import config
+
+API_key = API_USERNAME = config('API_key')
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]["q"] + "\n~" + json_data[0]["a"]
+    return quote
+
+
+@bot.event
+async def on_ready():
+    bot.load_extension('music')
+    print('Logged in succesfully.')
+    print(intents)
+  
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+
+@bot.command()
+async def quote(ctx):
+    quote = get_quote()
+    await ctx.send("```{0}```".format(quote))
+
+
+# @client.event
+# async def on_message(message):
+#     await client.process_commands(message)
+#     p = PerspectiveAPI(API_key)
+#     result = p.score(message.content)
+#     if isclose(result["TOXICITY"], 0.93, abs_tol=10**-1) or isclose(
+#             result["TOXICITY"], 0.99, abs_tol=10**-1) or isclose(
+#                 result["TOXICITY"], 0.75, abs_tol=10**-1) or isclose(
+#                     result["TOXICITY"], 1.0, abs_tol=10**-1):
+#         await message.channel.send("Seek help. Find god.")
+
+
+key = config('TOKEN')
+bot.run(key)
